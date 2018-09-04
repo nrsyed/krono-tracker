79cde0c515a8abb5625e0333e2d88c4461479ded
nrsyed <nsyed88@gmail.com>
2018-09-04 15:18:59 -0400

    Add files necessary for treating project as a package.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="krono-tracker",
+    version="0.1",
+    description="Simple terminal-based time tracking utility",
+    url="https://github.com/nrsyed/krono-tracker",
+    author="Najam R Syed",
+    author_email="najam.r.syed@gmail.com",
+    license="MIT",
+    packages=["krono"],
+    entry_points={
+        "console_scripts": ["krono = krono.__main__:main"]},
+    zip_safe=True)
