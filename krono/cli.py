299053c0bfdf809960d7e37420fcb0ca7671472f
nrsyed <nsyed88@gmail.com>
2018-09-04 16:20:19 -0400

    Cleaned up imports, added function documentation to cli.py

--- krono/cli.py
+++ krono/cli.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import cmd
-import curses
 import os
 import subprocess
 import sys
@@ -24,8 +23,25 @@ class CLI(cmd.Cmd):
     def preloop(self):
         clear()
         
+    def do_cd(self, arg):
+        """
+        Change to the given directory.
+
+        USAGE: setdir [../PATH/TO/NEW/DIRECTORY]
+        """
+
+        new_path = os.path.normpath(os.path.join(self.path, arg))
+        if os.path.isdir(new_path):
+            self.path = new_path
+            print(self.path)
+        else:
+            print("Error: The directory {} does not exist.".format(new_path))
 
     def do_create(self, arg):
+        """
+        Create a new log file. New file is not automatically loaded.
+        """
+
         filepath = os.path.abspath(arg)
         if not os.path.isfile(filepath):
             print("Creating database file {}".format(filepath))
@@ -38,14 +54,21 @@ class CLI(cmd.Cmd):
             print("Error: File already exists.")
 
     def do_exit(self, arg):
+        """Exit Krono Tracker."""
         return True
 
     def do_getdir(self, arg):
         """
-        Get the path to the currently active directory."""
+        Get the path to the currently active directory.
+        """
+
         print(self.path)
 
     def do_load(self, arg):
+        """
+        Load an existing log file.
+        """
+
         if arg == "":
             print("Error: No filename entered.")
             return
@@ -68,32 +91,24 @@ class CLI(cmd.Cmd):
     
     def do_ls(self, arg):
         """
-        List files in the currently active directory."""
+        List files in the currently active directory.
+        """
+
         subprocess.call("ls " + self.path, shell=True)
 
     def do_setcwd(self, arg):
         """
-        Set the active path to the current working directory (directory from
-        which this program is running).
+        Set the active path to the current working directory
+        (the directory in which you started running this program).
         """
+
         self.path = os.getcwd()
-
-    def do_setdir(self, arg):
-        """
-        USAGE: setdir [../PATH/TO/NEW/DIRECTORY]
-
-        Set the path for the active directory."""
-        new_path = os.path.normpath(os.path.join(self.path, arg))
-        if os.path.isdir(new_path):
-            self.path = new_path
-            print(self.path)
-        else:
-            print("Error: The directory {} does not exist.".format(new_path))
 
     def do_view(self, arg):
         """
         View the currently loaded log file.
         """
+
         if self.log is not None:
             formatted_rows = self.log.format_selected()
             if formatted_rows:
