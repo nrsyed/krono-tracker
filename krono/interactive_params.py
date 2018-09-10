7b5788827f0027af2d6cd5d10f14bd8f158b2739
nrsyed <nsyed88@gmail.com>
2018-09-10 17:10:16 -0400

    Change 'filters' to 'params'

--- krono/interactive_params.py
+++ krono/interactive_params.py
@@ -9,7 +9,7 @@ class InteractiveParams:
 
     def __init__(self, start="0000-01-01 00:00:00", end="9999-12-31 23:59:59",
                  project="", tags="", notes=""):
-        self.filters = OrderedDict([
+        self.params = OrderedDict([
             ("start", list(start)),
             ("end", list(end)),
             ("project", list(project)),
@@ -17,7 +17,7 @@ class InteractiveParams:
             ("notes", list(notes))
             ])
 
-        self.dict_keys = list(self.filters.keys())
+        self.dict_keys = list(self.params.keys())
         self.num_dict_keys = len(self.dict_keys)
 
         self.scr = None
@@ -36,24 +36,27 @@ class InteractiveParams:
             curses.echo()
             curses.curs_set(1)
             curses.endwin()
-        for dict_key in self.filters:
-            self.filters[dict_key] = "".join(self.filters[dict_key])
-        return dict(self.filters)
+        for dict_key in self.params:
+            self.params[dict_key] = "".join(self.params[dict_key])
+        return dict(self.params)
 
-    def _interactive_filter(self):
+    def _interactive_params(self):
         scr = self.scr
         scr_height, scr_width = scr.getmaxyx()
         scr.refresh()
         scr.keypad(True)
 
+        y_offset = 2
+        instructions = "Navigation [Up/Down/Left/Right/Tab], Select [Enter], Quit [q]"
+
         min_x = 10
-        date_separator_idx = [min_x + i for i, c in enumerate(self.filters["start"])
+        date_separator_idx = [min_x + i for i, c in enumerate(self.params["start"])
                               if c in ("-", " ", ":")]
-        date_len = len(self.filters["start"])
+        date_len = len(self.params["start"])
 
         def print_line(line_idx):
             dict_key = self.dict_keys[line_idx]
-            text = "{:<8}| {}".format(dict_key, "".join(self.filters[dict_key]))
+            text = "{:<8}| {}".format(dict_key, "".join(self.params[dict_key]))
             scr.move(line_idx, 0)
             scr.clrtoeol()
             scr.addstr(line_idx, 0, text)
@@ -73,7 +76,7 @@ class InteractiveParams:
 
         def move_right():
             dict_key = self.dict_keys[line]
-            if x < min_x + len(self.filters[dict_key]):
+            if x < min_x + len(self.params[dict_key]):
                 scr.move(line, x + 1)
 
         def move_left():
@@ -112,22 +115,22 @@ class InteractiveParams:
             elif key == curses.KEY_BACKSPACE:
                 if line > 1 and x > min_x:
                     dict_key = self.dict_keys[line]
-                    del self.filters[dict_key][x - min_x - 1]
+                    del self.params[dict_key][x - min_x - 1]
                     print_line(line)
                     move_left()
                 else:
                     date_move_left()
             elif line <= 1 and ord("0") <= key <= ord("9"):
                 dict_key = self.dict_keys[line]
-                self.filters[dict_key][x - min_x] = chr(key)
+                self.params[dict_key][x - min_x] = chr(key)
                 print_line(line)
                 date_move_right()
             elif line > 1 and 32 <= key <= 126:
                 dict_key = self.dict_keys[line]
-                if x - min_x < len(self.filters[dict_key]):
-                    self.filters[dict_key][x - min_x] = chr(key)
+                if x - min_x < len(self.params[dict_key]):
+                    self.params[dict_key][x - min_x] = chr(key)
                 else:
-                    self.filters[dict_key].append(chr(key))
+                    self.params[dict_key].append(chr(key))
                 print_line(line)
                 move_right()
             elif key == ord("\n"):
