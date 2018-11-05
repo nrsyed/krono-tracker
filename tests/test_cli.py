e226b0709bbb8d7bcb8ae62217a0eca99dbe43ec
nrsyed <nsyed88@gmail.com>
2018-11-04 19:00:49 -0500

    Add tests for path methods

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,7 @@ import sqlite3
 import pytest
 from krono.cli import CLI
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function")
 def cli():
     return CLI()
 
@@ -19,8 +19,10 @@ class TestCLI:
 
         # Test creation of new database.
         filepath = os.path.join(tmpdir.strpath, "new.db")
+        assert not os.path.isfile(filepath)
         cli.do_create(filepath)
         assert "Creating database file {}".format(filepath) in caplog.messages
+        assert os.path.isfile(filepath)
 
     def test_load(self, cli, caplog, database, tmpdir):
         """Test do_load()."""
@@ -47,3 +49,41 @@ class TestCLI:
         cli.do_load(new_filepath)
         assert cli.log
         assert cli.log.conn != previous_conn
+
+    def test_path(self, cli, capfd, caplog, tmpdir):
+        """Test CLI path navigation methods."""
+
+        # Check that initial path is correct.
+        assert cli.path == os.getcwd()
+
+        # Test getdir().
+        cli.do_getdir(None)
+        assert capfd.readouterr().out.rstrip() == os.getcwd()
+
+        # Test cd() on nonexistent directory.
+        nonexistent_dir = os.path.join(tmpdir.strpath, "nonexistentDir/")
+        cli.do_cd(nonexistent_dir)
+        error_msg = "The directory {} does not exist.".format(nonexistent_dir)
+        assert error_msg in caplog.messages
+
+        # Test cd() on existing directory.
+        cli.do_cd(tmpdir.strpath)
+        assert capfd.readouterr().out.rstrip() == tmpdir.strpath
+        assert cli.path == tmpdir.strpath
+
+        # Test ls() in empty directory.
+        cli.do_ls(None)
+        assert capfd.readouterr().out.rstrip() == ""
+
+        # Test ls() in a directory with a single file.
+        test_filename = "test"
+        test_filepath = os.path.join(tmpdir.strpath, test_filename)
+        with open(test_filepath, "w") as f:
+            pass
+        cli.do_ls(None)
+        assert capfd.readouterr().out.rstrip() == test_filename
+
+        # Test setcwd().
+        cli.do_setcwd(None)
+        assert cli.path == os.getcwd()
+        assert capfd.readouterr().out.rstrip() == os.getcwd()
